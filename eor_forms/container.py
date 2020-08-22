5d0f3d8f4d1c7e196f30b4f2d5f5040db755eeda
Thorn <p.thorn.ru@gmail.com>
2020-08-22 21:57:59 +0300

    Add List mapping

--- eor_forms/container.py
+++ eor_forms/container.py
@@ -5,7 +5,79 @@ log = logging.getLogger(__name__)
 
 
 class List(object):
-    pass
+    def __init__(self, name, child_factory):
+        self._name = name
+        self._child_factory = child_factory
+        self._children = []
+        self._validators = []  # validators or
+
+    @property
+    def name(self):
+        return self._name
+
+    # children
+
+    def __len__(self):
+        return len(self._children)
+
+    def __getitem__(self, key):
+        return self._children[key]  # will raise IndexError as expected
+
+    def append(self, child):
+        assert self._child is None  # TODO
+        self._child = child
+        return self
+
+    def removeAt(self, idx):
+        pass
+
+    # values
+
+    @property
+    def value(self):
+        return [child.value for child in self._children]
+
+    @value.setter
+    def value(self, val):
+        def child_with_value(val):
+            child = self._child_factory()
+            child.value = val
+            return child
+
+        self._children = [child_with_value(i) for i in val]
+
+    @property
+    def serialized_value(self):
+        return [child.serialized_value for child in self._children]
+
+    @serialized_value.setter
+    def serialized_value(self, val):
+        def child_with_value(val):
+            child = self._child_factory()
+            child.serialized_value = val
+            return child
+
+        self._children = [child_with_value(i) for i in val]
+
+    # validation
+
+    @property
+    def valid(self):
+        for validator in self._validators:
+            validator(self)
+            # TODO
+            # if self._errors:
+            #    break  # TODO
+
+        return all(child.valid for child in self._children)
+
+    @property
+    def error(self):
+        return {k: child.error for k, child in self._children.items() if not child.valid}
+
+    def validator(self, validator):
+        self._validators.append(validator)  # TODO revalidate?
+        return self
 
 
 class Mapping(object):
